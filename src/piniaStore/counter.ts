327877c4d5040ea9ef5b68b0a3368958e4c015d6
gaojuqian <jqiown@163.com>
2022-07-12 21:43:55 +0800

    1.添加 用户 pinia store

--- src/piniaStore/counter.ts
+++ src/piniaStore/counter.ts
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { reactive } from "vue";
 
 export const useCounterStore = defineStore("counter", {
     state: () => ({ count: 0 }),
@@ -7,4 +8,16 @@ export const useCounterStore = defineStore("counter", {
             this.count++;
         },
     },
+});
+
+export const useUserStore = defineStore("user", () => {
+    const userState = reactive({
+        info: {},
+    });
+
+    function setUserState({ type, stateValue }: { type: "info"; stateValue: any }) {
+        userState[type] = stateValue;
+    }
+
+    return { userState, setUserState };
 });
