bf8e159552411f18224d28ac32c2152535e58ba6
gaojuqian <jqiown@163.com>
2022-07-15 23:52:12 +0800

    1.添加 useRoute hook 2.keep-alive缓存 Home

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
-
-export default new VueRouter({
+const router = new VueRouter({
     routes: [
         // { name: "hello", path: "/hello", component: () => import("../components/HelloWorld") },
         { name: "Home", path: "/", component: () => import("@/page/Home/Home") },
         { name: "WebGpu", path: "/WebGpu", component: () => import("@/components/WebGpu") },
     ],
-});
+}) as VueRouter;
+export default router;
