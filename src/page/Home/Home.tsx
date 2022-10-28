d630f48cf974d2a109b91e2de0e5c7a4a58b5efc
cyRenShui <jqiown@163.com>
2022-10-28 20:03:55 +0800

    add three

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, toRefs, reactive, onMounted } from "vue";
+import {defineComponent, toRefs, reactive, onMounted, computed} from "vue";
 import { useCounterStore } from "@src/piniaStore/counter";
 import HelloWorld from "@src/components/HelloWorld";
 import { useRouter } from "@src/util/myHook";
@@ -6,20 +6,22 @@ import { useRouter } from "@src/util/myHook";
 export default defineComponent({
     name: "Home",
     setup(props, context) {
-        const { increment } = useCounterStore();
+        const store = useCounterStore()
         const router = useRouter();
 
         const state = reactive({});
         return {
             ...toRefs(state),
-            increment,
+            increment: store.increment,
             router,
+            count: computed(()=> store.count)
         };
     },
     render() {
         const { count, router } = this;
         return (
             <div style={"font-size: 14px"}>
+                {count}
                 <div style={"font-size: 24px;"}>这是一段没用的文字</div>
                 <h1>这是一段没用的文字</h1>
                 <button onClick={() => router.push("/WebGpu")}>WebGpu</button>
